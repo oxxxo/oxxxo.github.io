c7dfc097128e91f12262a7d1cb255dfa9562822f
oxxxo <oxxxo@users.noreply.github.com>
2018-04-18 07:38:19 +0300

    Update text.js

--- txt/text/text.js
+++ txt/text/text.js
@@ -1,12 +1,31 @@
 var passage = `
  
-         hgfhgfjgjyjt
- hgfhgfjgjyjt    hgfhgfjgjyjt
- hgfhgfjgjyjt    hgfhgfjgjyjt
- hgfhgfjgjyjt    hgfhgfjgjyjt
-        hgfhgfjgjyjt
-        абв где жзи
-        абв где жзи
+         hgfhgfjgjyjt                       hgfhgfjgjyjt
+ hgfhgfjgjyjt    hgfhgfjgjyjt       hgfhgfjgjyjt    hgfhgfjgjyjt
+ hgfhgfjgjyjt    hgfhgfjgjyjt       hgfhgfjgjyjt    hgfhgfjgjyjt
+ hgfhgfjgjyjt    hgfhgfjgjyjt       hgfhgfjgjyjt    hgfhgfjgjyjt
+         hgfhgfjgjyjt                       hgfhgfjgjyjt
+        
+                           абв где жзи
+                           абв где жзи
+
+         hgfhgfjgjyjt                       hgfhgfjgjyjt
+ hgfhgfjgjyjt    hgfhgfjgjyjt       hgfhgfjgjyjt    hgfhgfjgjyjt
+ hgfhgfjgjyjt    hgfhgfjgjyjt       hgfhgfjgjyjt    hgfhgfjgjyjt
+ hgfhgfjgjyjt    hgfhgfjgjyjt       hgfhgfjgjyjt    hgfhgfjgjyjt
+         hgfhgfjgjyjt                       hgfhgfjgjyjt
+        
+                           абв где жзи
+                           абв где жзи
+
+         hgfhgfjgjyjt                       hgfhgfjgjyjt
+ hgfhgfjgjyjt    hgfhgfjgjyjt       hgfhgfjgjyjt    hgfhgfjgjyjt
+ hgfhgfjgjyjt    hgfhgfjgjyjt       hgfhgfjgjyjt    hgfhgfjgjyjt
+ hgfhgfjgjyjt    hgfhgfjgjyjt       hgfhgfjgjyjt    hgfhgfjgjyjt
+         hgfhgfjgjyjt                       hgfhgfjgjyjt
+        
+                           абв где жзи
+                           абв где жзи
 
 
  `
