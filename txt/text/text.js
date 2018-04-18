af4b10e7b9c08fad65c77b8110ce91a34fb6d1af
oxxxo <oxxxo@users.noreply.github.com>
2018-04-18 07:50:29 +0300

    Update text.js

--- txt/text/text.js
+++ txt/text/text.js
@@ -1,43 +1,11 @@
 var passage = `
 ---------------------------------------------------------------
-
-         hgfhgfjgjyjt                    hgfhgfjgjyjt
- hgfhgfjgjyjt     hgfhgfjgjyjt : hgfhgfjgjyjt     hgfhgfjgjyjt
- hgfhgfjgjyjt     hgfhgfjgjyjt : hgfhgfjgjyjt     hgfhgfjgjyjt
- hgfhgfjgjyjt     hgfhgfjgjyjt : hgfhgfjgjyjt     hgfhgfjgjyjt
-         hgfhgfjgjyjt                    hgfhgfjgjyjt
-
+                  hgfhgfjgjyjt : hgfhgfjgjyjt
+                  hgfhgfjgjyjt : hgfhgfjgjyjt
+                  hgfhgfjgjyjt : hgfhgfjgjyjt
+                  hgfhgfjgjyjt : hgfhgfjgjyjt
 ---------------------------------------------------------------
         
-                           абв где жзи
-                           абв где жзи
-
-         hgfhgfjgjyjt                       hgfhgfjgjyjt
- hgfhgfjgjyjt    hgfhgfjgjyjt       hgfhgfjgjyjt    hgfhgfjgjyjt
- hgfhgfjgjyjt    hgfhgfjgjyjt       hgfhgfjgjyjt    hgfhgfjgjyjt
- hgfhgfjgjyjt    hgfhgfjgjyjt       hgfhgfjgjyjt    hgfhgfjgjyjt
-         hgfhgfjgjyjt                       hgfhgfjgjyjt
-        
-                           абв где жзи
-                           абв где жзи
-
-         hgfhgfjgjyjt                       hgfhgfjgjyjt
- hgfhgfjgjyjt    hgfhgfjgjyjt       hgfhgfjgjyjt    hgfhgfjgjyjt
- hgfhgfjgjyjt    hgfhgfjgjyjt       hgfhgfjgjyjt    hgfhgfjgjyjt
- hgfhgfjgjyjt    hgfhgfjgjyjt       hgfhgfjgjyjt    hgfhgfjgjyjt
-         hgfhgfjgjyjt                       hgfhgfjgjyjt
-        
-                           абв где жзи
-                           абв где жзи
-
-         hgfhgfjgjyjt                       hgfhgfjgjyjt
- hgfhgfjgjyjt    hgfhgfjgjyjt       hgfhgfjgjyjt    hgfhgfjgjyjt
- hgfhgfjgjyjt    hgfhgfjgjyjt       hgfhgfjgjyjt    hgfhgfjgjyjt
- hgfhgfjgjyjt    hgfhgfjgjyjt       hgfhgfjgjyjt    hgfhgfjgjyjt
-         hgfhgfjgjyjt                       hgfhgfjgjyjt
-        
-                           абв где жзи
-                           абв где жзи
 
 
  `
